b2d326875f078506c0935d5e6b347d5114901693
V! <125708505+Vitor-Lucas@users.noreply.github.com>
2024-04-02 11:45:09 -0300

    Funcionalidade da Câmera

--- app/src/main/java/com/example/camera_test/MainActivity.kt
+++ app/src/main/java/com/example/camera_test/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.camera_test
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imageView: ImageView
+    private val REQUEST_IMAGE_CAPTURE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        imageView = findViewById(R.id.imageView)
+        val buttonTakePhoto: Button = findViewById(R.id.button_take_photo)
+        val buttonDeletePhoto: Button = findViewById(R.id.button_delete_photo)
+
+        buttonTakePhoto.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (takePictureIntent.resolveActivity(packageManager) != null) {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        }
+
+        buttonDeletePhoto.setOnClickListener {
+            imageView.setImageDrawable(null)
+        }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as? Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+    }
+
 }
